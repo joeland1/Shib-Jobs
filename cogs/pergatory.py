d2a29674c9e89b1fb312fc9281a1d2f79959e2d4
joeland1 <55666119+joeland1@users.noreply.github.com>
2020-11-30 08:39:41 -0500

    added absolute verification

--- cogs/pergatory.py
+++ cogs/pergatory.py
@@ -10,23 +10,36 @@ import global_functions
 class Pergatory(commands.Cog):
     def __init__(self, bot):
             self.bot=bot
+            self.verify_all=None
     #@commands.Cog.listener() -> use for events like on_ready
     @commands.Cog.listener()
     async def on_ready(self):
         if config.PERG_HOUR>=24:
             print("Change your pergatory hours to proper conversion. It's too big") #niceeeeeeeeeee
             sys.exit(4)
+
+
+    #based on time
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        time_difference = datetime.utcnow()-member.created_at
-        if time_difference.days > config.PERG_DAY and time_difference.seconds//3600 > config.PERG_HOUR:
-            #old account
-            await self.bot.get_channel(config.PERG_LOG).send(member.mention+" has joined")
-
-        else:
+        if self.verify_all is None:
+            print("error verify cog")
+            sys.exit(4)
+        if self.verify_all=True:
             perg_add_role=discord.utils.get(member.guild.roles, id=config.PERG_ROLE_ID)
             await member.add_roles(perg_add_role)
             await self.bot.get_channel(config.PERG_LOG).send(member.mention+" is locked in perg")
+
+        elif:  
+            time_difference = datetime.utcnow()-member.created_at
+            if time_difference.days > config.PERG_DAY and time_difference.seconds//3600 > config.PERG_HOUR:
+                #old account
+                await self.bot.get_channel(config.PERG_LOG).send(member.mention+" has joined")
+
+            else:
+                perg_add_role=discord.utils.get(member.guild.roles, id=config.PERG_ROLE_ID)
+                await member.add_roles(perg_add_role)
+                await self.bot.get_channel(config.PERG_LOG).send(member.mention+" is locked in perg")
 
     @commands.command()
     async def verify(self, ctx, members: commands.Greedy[discord.Member]):
